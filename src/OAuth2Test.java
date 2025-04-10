ccd71e3cf271c00ef9a4e7f20c97511fd8afa07a
QA-Shivam <shivamkumar16399@gmail.com>
2025-04-10 17:55:12 +0530

    pass remove

--- src/OAuth2Test.java
+++ src/OAuth2Test.java
@@ -15,9 +15,9 @@ public static void main(String[] args) throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php&state=verifyrsa");
 	String code= driver.getCurrentUrl();
-	driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys("shivamkumar16399@gmail.com");
+	driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys("shivamkumar.com");
 	driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys(Keys.ENTER);
-	driver.findElement(By.xpath("//input[@aria-label='Enter your password']")).sendKeys("Coder@99");
+	driver.findElement(By.xpath("//input[@aria-label='Enter your password']")).sendKeys("");
 	driver.findElement(By.xpath("//input[@aria-label='Enter your password']")).sendKeys(Keys.ENTER);
 	Thread.sleep(2000);
 	String url= driver.getCurrentUrl();
